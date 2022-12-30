7b4e8f87b96ef14452a005b880dd0180ab1f0792
dmikhaylov <d.y.mikhaylov@ya.ru>
2022-12-31 01:47:43 +0400

    removed class constructor

--- src/test/java/hometask/base/Base.java
+++ src/test/java/hometask/base/Base.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Base {
-
-    public Base(String url) {
-        open(url);
-    }
+abstract public class Base {
 
     private void setUp() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @BeforeEach
