771e54399ce64d243b01868ec2d3c16844d126b4
dmikhaylov <d.y.mikhaylov@ya.ru>
2023-01-05 22:03:08 +0400

    added pageLoadStrategy

--- src/test/java/hometask/base/Base.java
+++ src/test/java/hometask/base/Base.java
@@ -9,9 +9,10 @@ abstract public class Base {
 
     private void setUp() {
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-//        Configuration.browserSize = "1366x768";
+//        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1366x768";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @BeforeEach
